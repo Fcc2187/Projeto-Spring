5dce3c47bb0db17b6ed850d05359f187206a0e8e
Fcc2187 <fcc3@cesar.school>
2024-11-13 17:16:39 -0300

    Get game by id done

--- src/main/java/com/devcamisa/dslist/controllers/GameController.java
+++ src/main/java/com/devcamisa/dslist/controllers/GameController.java
@@ -6,21 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcamisa.dslist.services.GameService;
+import com.devcamisa.dslist.dto.GameDTO;
 import com.devcamisa.dslist.dto.GameMinDTO;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 //The controllers are responsible for receiving the requests and returning the responses
 @RestController
 @RequestMapping(value = "/games")//Defines the base URL for the controller(simmilar to the views in Django)
 public class GameController {
     @Autowired
-    private GameService service;
+    private GameService gameService;
+
+    //Method to handle GET requests to /games/{id}
+    @GetMapping(value = "/{id}") 
+    public GameDTO findById(@PathVariable Long id) {
+        //Calls the GameService to find a game by its ID
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping //Annotation to define that this method is a GET request(Simmilar to Django) 
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> list = service.findAll();
-        return list;
+        //Calls the GameService to get all games
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
     }
 }
--- src/main/java/com/devcamisa/dslist/services/GameService.java
+++ src/main/java/com/devcamisa/dslist/services/GameService.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import com.devcamisa.dslist.repositories.GameRepository;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devcamisa.dslist.dto.GameDTO;
 import com.devcamisa.dslist.dto.GameMinDTO;
 import com.devcamisa.dslist.entities.Game;
 
@@ -16,6 +19,13 @@ public class GameService {
     @Autowired //Injects the dependency automatically
     private GameRepository gameRepository;
     
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get(); //get a Game by id 
+        return new GameDTO(result); //returns the result and converts Game to GameDTO
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll(); //Returns a list of Game entities
         return result.stream().map(x -> new GameMinDTO(x)).toList(); //Converts the list of Games entities to a list of GameMinDTO(Excluding some attributes)
